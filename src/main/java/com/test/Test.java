3666818a50889dfb51d4a88afd0da969e4717750
luoxr <luoxr@paoche56.com>
2019-12-18 13:26:20 +0800

    first

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author luoxr
+ * @date 2019/12/16 13:16
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "18710006370";
+        System.out.println(s.substring(s.length() -4));
+    }
+}
